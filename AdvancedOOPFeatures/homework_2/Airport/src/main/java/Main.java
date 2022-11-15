8c4c40841b7b79b967879881fe83ea5b19d1d85d
Igor <belfegorum@list.ru>
2022-11-16 00:13:01 +1000

    Homework at 15/11/2022

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,35 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        Airport airport = Airport.getInstance();
+        List<Flight> lanes = findPlanesLeavingInTheNextTwoHours(airport);
 
+        System.out.println("Список рейсов");
+        lanes.stream().forEach(System.out::println);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
 
+        Date dateFlight = new Date(System.currentTimeMillis() + 60 * 60 * 2 * 1000);
+        Date now = new Date();
+        ArrayList<Flight> flightList = new ArrayList<>();
+
+        airport.getTerminals()
+                .stream().forEach(terminal -> {
+                    terminal.getFlights().stream().filter(flight ->
+                                    flight.getDate().compareTo(dateFlight) <= 0 &&
+                                            flight.getDate().compareTo(now) >= 0 &&
+                                            flight.getType() == Flight.Type.DEPARTURE)
+                            .forEach(flightList::add);
+                });
+
+        return flightList;
+    }
 }
